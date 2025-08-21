82f608da340a994090bab91d931ba5666c4a0a77
Billal GHILAS <bilal@evercam.io>
2025-08-21 13:38:10 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,25 @@
 defmodule HLX.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github_url "https://github.com/gBillal/hlx"
+
   def project do
     [
       app: :hlx,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # hex
+      description: "HLS reader and writer",
+      package: package(),
+
+      # docs
+      name: "HLS Reader and Writer",
+      source_url: @github_url,
+      docs: docs()
     ]
   end
 
@@ -25,6 +37,25 @@ defmodule HLX.MixProject do
       {:mpeg_ts, github: "gBillal/kim_mpeg_ts", ref: "d8421f1"},
       {:qex, "~> 0.5.1"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Billal Ghilas"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => @github_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md", "LICENSE"],
+      formatters: ["html"],
+      source_ref: "v#{@version}"
     ]
   end
 end
