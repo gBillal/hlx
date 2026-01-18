211eaf3e77ae25533af890585b569f6f2bc3b808
Billal GHILAS <bilal@evercam.io>
2026-01-18 11:30:47 +0100

    Bunp min elixir version to 1.16

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule HLX.MixProject do
     [
       app: :hlx,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
